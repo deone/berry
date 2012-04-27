ebd8c6f30bf36916732baf18a7b5f6faa12963ac
Oladayo Osikoya <alwaysdeone@gmail.com>
2012-04-27 12:19:28 +0100

    Remove latest_update_at field from Member. Model instance should check whether is_qualified and is_standard, is_bronze, is_silver, is_gold, is_platinum or is_diamond. Add Rank class to set the member's rank. Add RANKS options and change referrer related_name to 'referrals' from 'referrees'.

--- refer/models.py
+++ refer/models.py
@@ -1,22 +1,96 @@
 from django.db import models
-from django.contrib.auth.models import get_hexdigest
-from django.contrib.auth.models import check_password
 
 from mptt.models import MPTTModel, TreeForeignKey
 
-import datetime
+from datetime import datetime
 import random
 
+RANKS = (
+	(1, "Standard"),
+	(2, "Bronze"),
+	(3, "Silver"),
+	(4, "Gold"),
+	(5, "Platinum"),
+	(6, "Diamond"),
+	)
 
 class Member(MPTTModel):
     subscriber = models.OneToOneField('accounts.SubscriberInfo')
-    referrer = models.ForeignKey('self', null=True, blank=True, related_name='referrees')
+    referrer = models.ForeignKey('self', null=True, blank=True,
+	    related_name='referrals')
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
-    date_joined = models.DateTimeField(default=datetime.datetime.now)
-    latest_update_at = models.DateTimeField(default=datetime.datetime.now)
+    date_joined = models.DateTimeField(default=datetime.now)
 
     def __unicode__(self):
 	return str(self.subscriber.msisdn)
 
+    def is_qualified(self):
+	if self.referrals.count() >= 2:
+	    return True
+	else:
+	    return False
+
+    def is_standard(self):
+	if (self.get_descendants().filter(level=self.level + 1).count() == 2)\
+	and self.rank.rank == 0:
+	    self.rank.rank = 1
+	    self.rank.save()
+	    return True
+	else:
+	    return False
+
+    def is_bronze(self):
+	if (self.get_descendants().filter(level=self.level + 2).count() == 4)\
+	and self.rank.rank == 1:
+	    self.rank.rank = 2
+	    self.rank.save()
+	    return True
+	else:
+	    return False
+
+    def is_silver(self):
+	if (self.get_descendants().filter(level=self.level + 3).count() == 8)\
+	and self.rank.rank == 2:
+	    self.rank.rank = 3
+	    self.rank.save()
+	    return True
+	else:
+	    return False
+
+    def is_gold(self):
+	if (self.get_descendants().filter(level=self.level + 4).count() == 16)\
+	and self.rank.rank == 3:
+	    self.rank.rank = 4
+	    self.rank.save()
+	    return True
+	else:
+	    return False
+
+    def is_platinum(self):
+	if (self.get_descendants().filter(level=self.level + 5).count() == 32)\
+	and self.rank.rank == 4:
+	    self.rank.rank = 5
+	    self.rank.save()
+	    return True
+	else:
+	    return False
+
+    def is_diamond(self):
+	if (self.get_descendants().filter(level=self.level + 6).count() == 64)\
+	and self.rank.rank == 5:
+	    self.rank.rank = 6
+	    self.rank.save()
+	    return True
+	else:
+	    return False
+
     class MPTTMeta:
 	order_insertion_by = ['subscriber']
+
+
+class Rank(models.Model):
+    member = models.OneToOneField(Member)
+    rank = models.IntegerField(default=0, choices=RANKS)
+
+    def __unicode__(self):
+	return str(self.rank)
